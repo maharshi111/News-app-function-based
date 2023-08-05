df0ce64790e07c5c49b0f47e1f900509ef296cc0
maharshi111 <97938886+maharshi111@users.noreply.github.com>
2023-08-06 04:55:34 +0530

    changed app.js to function base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState} from 'react'
 import NavBar from './Components/NavBar';
 import News  from './Components/News';
 import {
@@ -15,18 +15,20 @@ import {
 import LoadingBar from 'react-top-loading-bar'
 
  //const pNo = 15 we can use const key word outside of the class and than in Route use pageSize= {pNo}
-export default class App extends Component {
-  pNo = 15 // can't use const or let inside a class , so here we are using this keyword as a reference
+function App() {
+  const pNo = 15 // can't use const or let inside a class , so here we are using this keyword as a reference
   
-  state = {
-    progress: 0
-  }
+  // state = {
+  //   progress: 0
+  // }
+  const[progress,setProgress] = useState(0)
 
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
-  }
+  // setProgress=(progress)=>{
+  //  this.setState({progress:progress})
+  // }
+ 
 
-  render() {
+
     return ( 
       
       <div>
@@ -34,23 +36,24 @@ export default class App extends Component {
           <NavBar />
           <LoadingBar
             color='#f11946'
-            progress={this.state.progress}
+            progress={progress}
             height={3}  
           />
           <Routes>
-            <Route exact path='/' element={<News setProgress={this.setProgress}  key='general' pageSize= {this.pNo} country='us' category='general' />}></Route>
-            <Route exact path='/business' element={<News setProgress={this.setProgress}  key='business' pageSize={this.pNo} country='us' category='business' />}></Route>
-            <Route exact path='/entertainment' element={<News setProgress={this.setProgress}  key='entertainment' pageSize={this.pNo} country='us' category='entertainment' />}></Route>
-            <Route exact path='/general' element={<News setProgress={this.setProgress}  key='general' pageSize={this.pNo} country='us' category='general' />}></Route>
-            <Route exact path='/health' element={<News setProgress={this.setProgress}  key='health' pageSize={this.pNo} country='us' category='health' />}></Route>
-            <Route exact path='/science' element={<News setProgress={this.setProgress}  key='science' pageSize={this.pNo} country='us' category='science' />}></Route>
-            <Route exact path='/sports' element={<News setProgress={this.setProgress}  key='sports' pageSize={this.pNo} country='us' category='sports' />}></Route>
-            <Route exact path='/technology' element={<News setProgress={this.setProgress}  key='technology' pageSize={this.pNo} country='us' category='technology' />}></Route>
+            <Route exact path='/' element={<News setProgress={setProgress}  key='general' pageSize= {pNo} country='us' category='general' />}></Route>
+            <Route exact path='/business' element={<News setProgress={setProgress}  key='business' pageSize={pNo} country='us' category='business' />}></Route>
+            <Route exact path='/entertainment' element={<News setProgress={setProgress}  key='entertainment' pageSize={pNo} country='us' category='entertainment' />}></Route>
+            <Route exact path='/general' element={<News setProgress={setProgress}  key='general' pageSize={pNo} country='us' category='general' />}></Route>
+            <Route exact path='/health' element={<News setProgress={setProgress}  key='health' pageSize={pNo} country='us' category='health' />}></Route>
+            <Route exact path='/science' element={<News setProgress={setProgress}  key='science' pageSize={pNo} country='us' category='science' />}></Route>
+            <Route exact path='/sports' element={<News setProgress={setProgress}  key='sports' pageSize={pNo} country='us' category='sports' />}></Route>
+            <Route exact path='/technology' element={<News setProgress={setProgress}  key='technology' pageSize={pNo} country='us' category='technology' />}></Route>
           </Routes>
         </Router>
       </div>
  
     )
-  }
+  
 }
 
+export default App
